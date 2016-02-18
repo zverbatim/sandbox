f23a5250ffcd8eec609c07d0f06f3c1bad60f461
zverbatim <imunteanu@unomaha.edu>
2016-02-18 17:32:00 -0600

    swift controlflow and start looking @ clases

---
+++ swift/playground/classes/Contents.swift
@@ -0,0 +1,56 @@
+/*
+    SWIFT - Classes
+*/
+
+import UIKit
+
+var str = "Hello, playground"
+
+class Cup {
+    var price : Int?
+    
+    func identityString() {
+        print("Cup selling for \(self.price)")
+    }
+}
+
+var cup1 = Cup()
+let cup2 = Cup()
+cup2.price = 100
+cup2.identityString()
+
+struct Resolution {
+    var width = 0
+    var height = 0
+}
+
+class VideoMode {
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name: String?
+}
+
+let someResolution = Resolution()
+var someVideoMode1 = VideoMode()
+var someVideoMode2 = VideoMode()
+
+someVideoMode1.interlaced = true
+
+
+// class with initializer
+class Pot {
+    // ?  mean it's optional
+    var volume : Int?
+    
+    // with default value
+    var price : Int = 100
+    
+    // set the value in init()
+    var location : String
+    
+    init() {
+        location = "Greece"
+    }
+}
+
---
+++ swift/playground/controlflow/Contents.swift
@@ -0,0 +1,93 @@
+/*
+    SWIFT - control flow
+*/
+
+import UIKit
+
+var currency = ("USD", 100)
+
+switch currency{
+    case ("EUR", _):
+        print("it' EUR")
+    case ("USD", 200):
+        print("USD  = 200")
+    case (_, 100):
+        print("it's a 100")
+    default:
+        print("hz")
+}
+
+
+var count = 100
+
+switch count {
+case 0...50:
+    print("from 0 to 50")
+default:
+    print("anything else")
+}
+
+
+func hello(){
+    print("Hello")
+}
+hello()
+
+
+func addTwo(z : Int, alpha a : Int, b : Int) -> Int {
+    return z + a + b
+}
+
+var c = addTwo(100, alpha: 1, b:3)
+
+
+func addMany(numbers : Int ...) -> Int {
+    var tempSum = 0
+    for i in numbers {
+        tempSum += i
+    }
+    
+    return tempSum
+}
+
+var s = addMany(1,2,3,4,5)
+
+
+// functional stuff
+func createAdder(n : Int) -> Int -> Int {
+    func adder(addedNumber : Int) -> Int {
+        return n + addedNumber
+    }
+    
+    return adder
+}
+
+var addTen = createAdder(100)
+var addResult = addTen(5)
+
+func passFunctionAsParam(n : Int, doSomething : (Int) -> Int ) -> Int {
+    return doSomething(n)
+}
+
+passFunctionAsParam(10,  doSomething: addTen)
+
+
+// closures
+var sales  = [100, 120, 5, 20]
+sales.sort { (a, b) -> Bool in
+    b < a
+}
+
+sales.filter { (s) -> Bool in
+    s > 20
+}
+
+
+
+
+
+
+
+
+
+
