20b84ba1f36d02811818989d969e01a5187afcbd
zverbatim <imunteanu@unomaha.edu>
2016-03-27 13:59:21 -0500

    checkbox and text filter work independently

--- react/public/app/product.js
+++ react/public/app/product.js
@@ -1,49 +1,72 @@
 var data = [
-    {"id": 1, "name": "snowmen", "price": 123},
-    {"id": 2, "name": "shovel", "price": 30},
-    {"id": 3, "name": "carrot", "price": 5},
-    {"id": 4, "name": "comb", "price": 12}
+    {"id": 1, "name": "snowmen", "price": 123, "available": "true"},
+    {"id": 2, "name": "shovel", "price": 30, "available": "false"},
+    {"id": 3, "name": "carrot", "price": 5, "available": "false"},
+    {"id": 4, "name": "comb", "price": 12, "available": "true"}
 ];
 
 var ProductBox = React.createClass({
-    getInitialState: function () {
-        return {data: this.props.data};
-    },
+        getInitialState: function () {
+            return {data: this.props.data};
+        },
 
-    filterProduct: function (filterTerm) {
-        if (filterTerm) {
-            // will search always against the entire set
-            var filterData = _.filter(this.props.data, function (productItem) {
-                return _.contains(productItem.name + productItem.price, filterTerm)
-            });
+        filterText: function (filterTerm) {
+            if (filterTerm) {
+                return _.filter(this.state.data, function (productItem) {
+                    var allStrings = (productItem.name + productItem.price).toLowerCase();
+                    return allStrings.search(filterTerm.toLowerCase()) >= 0;
+                })
+            } else {
+                // if empty string list all data
+                return this.props.data
+            }
+        },
+
+        filterCheckBox: function (filterTerm) {
+            if (filterTerm) {
+                return _.filter(this.state.data, function (productItem) {
+                    return productItem.available === "true"
+                });
+            } else {
+                //if not checked all data
+                return this.props.data
+            }
+        },
+
+        filterProduct: function (event) {
+            var filterData = [];
+            if (event.target.type == "text") {
+                filterData = this.filterText(event.target.value);
+            } else if (event.target.type == "checkbox") {
+                filterData = this.filterCheckBox(event.target.checked);
+            }
+
             this.setState({data: filterData});
-        } else {
-            // list all product if no search term
-            this.setState({data: this.props.data});
-        }
-    },
+            console.log("---");
+            console.log(filterData);
+        },
 
-    render: function () {
-        return (
-            <div className="productBox">
-                <h1>
-                    Product Box
-                </h1>
-                <ProductForm filterProduct={this.filterProduct}/>
-                <ProductList data={this.state.data}/>
-            </div>
-        )
-    }
-});
+        render: function () {
+            return (
+                <div className="productBox">
+                    <h1> Product Box </h1>
+                    <ProductForm filterProduct={this.filterProduct}/>
+                    <ProductList data={this.state.data}/>
+                </div>
+            )
+        }
+    })
+    ;
 
 var ProductForm = React.createClass({
     filter: function (e) {
-        this.props.filterProduct(e.target.value.trim());
+        this.props.filterProduct(e)
     },
     render: function () {
         return (
             <form className="productForm" onChange={this.filter}>
                 <input type="text" placeholder="product filter..."/>
+                <input type="checkbox"/>available
             </form>
         )
     }
@@ -58,7 +81,16 @@ var ProductList = React.createClass({
         });
         return (
             <div className="productList">
-                {productNodes}
+                <table>
+                    <thead>
+                    <th>name</th>
+                    <th>price</th>
+                    <th>available</th>
+                    </thead>
+                    <tbody>
+                    {productNodes}
+                    </tbody>
+                </table>
             </div>
         )
     }
@@ -67,9 +99,11 @@ var ProductList = React.createClass({
 var Product = React.createClass({
     render: function () {
         return (
-            <div className="product">
-                {this.props.product.name} - {this.props.product.price}
-            </div>
+            <tr>
+                <td>{this.props.product.name}</td>
+                <td>{this.props.product.price}</td>
+                <td>{this.props.product.available}</td>
+            </tr>
         )
     }
 });
