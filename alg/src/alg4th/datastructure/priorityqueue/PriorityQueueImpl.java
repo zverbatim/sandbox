2aa55bc271b0201012d856af4fc2617e5a60e84d
zverbatim <imunteanu@unomaha.edu>
2016-04-08 17:34:18 -0500

    implement and debugg a priority queue

--- alg/src/alg4th/datastructure/priorityqueue/PriorityQueueImpl.java
+++ alg/src/alg4th/datastructure/priorityqueue/PriorityQueueImpl.java
@@ -1,7 +1,34 @@
 package alg4th.datastructure.priorityqueue;
 
-/**
- * Created by imunteanu on 4/8/16.
- */
+import edu.princeton.cs.algs4.MaxPQ;
+
 public class PriorityQueueImpl {
+
+    private static void print(MaxPQ pq) {
+        for (Object s : pq) {
+            System.out.println(s + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        // init
+        MaxPQ<String> pq = new MaxPQ<>();
+        System.out.println("Intial size: " + pq.size());
+
+        // insert delete the max
+        pq.insert("K");
+        pq.insert("L");
+        pq.insert("N");
+        pq.insert("M");
+        pq.insert("S");
+        pq.delMax();
+        pq.delMax();
+        pq.insert("T");
+        String max = pq.delMax();
+        System.out.println("Deleted max " + max);
+        System.out.println("Current max = " + pq.max());
+        print(pq);
+    }
 }
