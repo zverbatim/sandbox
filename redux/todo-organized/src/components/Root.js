8be53e02e7ee3083e78d1a0ab1b5c0ed724b4f96
zverbatim <imunteanu@unomaha.edu>
2016-12-21 21:13:01 -0600

    add Router, Route and browserHistory

--- redux/todo-organized/src/components/Root.js
+++ redux/todo-organized/src/components/Root.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {PropTypes} from "react";
 import { Provider } from 'react-redux'
+import {Route, Router, browserHistory} from 'react-router';
 import App from './App'
 
 const Root = ({store}) => {
     return (
         <Provider store={store}>
-            <App />
+            <Router history={browserHistory}>
+                <Route path="/" component={App}/>
+            </Router>
         </Provider>
     )
+};
+
+Root.propTypes={
+    store: PropTypes.object.isRequired
 };
 
 export default Root;
