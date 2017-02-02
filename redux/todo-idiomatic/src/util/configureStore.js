d1d067de6e6adfc874fba0126657108daf913a51
zverbatim <imunteanu@unomaha.edu>
2017-02-02 11:38:13 -0600

    add redux-promise, redux-logger middlewares

--- redux/todo-idiomatic/src/util/configureStore.js
+++ redux/todo-idiomatic/src/util/configureStore.js
@@ -1,46 +1,23 @@
 /**
  * Function to handle the store creation
  */
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
+import promise from 'redux-promise'
+import createLogger from 'redux-logger'
 import reducer from "../reducers/index";
 
-const logger = (store) => (next) => (action) => {
-    console.group(action.type)
-    console.log("%c prev state: ", 'color: gray', store.getState())
-    console.log("%c action: ", 'color: green', action)
-    const returnValue = next(action)
-    console.log("%c next state: ", 'color: blue', store.getState())
-    console.groupEnd(action.type)
-    return returnValue
-}
-
-const promise = (store) => (next) => (action) => {
-    if (typeof action.then === 'function') {
-        return action.then(next)
-    }
-    return next(action)
-}
-
-const addMiddleWaresToDispatch = (store, middleWares) => {
-    console.log("middleWares", middleWares)
-    middleWares.slice().reverse().forEach(middleWare =>
-        store.dispatch = middleWare(store)(store.dispatch)
-    )
-}
-
 const configureStore = () => {
-
-    const store = createStore(reducer);
     const middleWares = [promise]
 
     // enable logging for dev
     if (process.env.NODE_ENV !== 'production') {
-        middleWares.push(logger)
+        middleWares.push(createLogger())
     }
 
-    addMiddleWaresToDispatch(store, middleWares)
-
-    return store;
+    return createStore(
+        reducer,
+        applyMiddleware(...middleWares)
+    );
 };
 
 export default configureStore;
