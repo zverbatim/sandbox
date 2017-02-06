59d4ca587656a74912d704adb6fe5c994631f046
zverbatim <imunteanu@unomaha.edu>
2017-02-06 11:51:46 -0600

    using fetch for - load with GET - add with POST - update with PUT - destroy with DELETE

--- react/react-prod/README.md
+++ react/react-prod/README.md
@@ -1,2 +1,10 @@
 # react-production-ready
 tutorial based of https://egghead.io/lessons/react-render-a-react-ui-with-jsx
+
+to run the json server 
+```bash
+npm run server
+```
+it will run a server that will serve json configured in `db.json` on port 8080
+to test `curl localhost:8080/todos`
+
--- react/react-prod/src/App.js
+++ react/react-prod/src/App.js
@@ -1,17 +1,14 @@
 import React, {Component} from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import {TodoForm, TodoList, Footer} from "./components/todo";
-import {addTodo, findById, generatedId, toggleTodo, updateTodo, deleteTodo, filterTodo} from "./lib/todoHelpers";
+import {Footer, TodoForm, TodoList} from "./components/todo";
+import {addTodo, deleteTodo, filterTodo, findById, generatedId, toggleTodo, updateTodo} from "./lib/todoHelpers";
 import {partial, pipe} from "./lib/utils";
+import {createTodo, destroyTodo, loadTodos, saveTodo} from "./lib/todoService";
 
 class App extends Component {
     state = {
-        todos: [
-            {id: 1, name: 'aloha', isComplete: true},
-            {id: 2, name: 'hello', isComplete: false},
-            {id: 3, name: 'buna ziua', isComplete: false}
-        ],
+        todos: [],
         currentTodo: '',
         errorMessage: ''
     }
@@ -34,12 +31,24 @@ class App extends Component {
             isComplete: false,
             id: id
         }
+
         const updatedTodos = addTodo(this.state.todos, todo)
         this.setState({
             currentTodo: '',
             todos: updatedTodos,
             errorMessage: ''
         })
+
+        createTodo(todo)
+            .then(() => this.showTempMessage('Todo Added'))
+    }
+
+    showTempMessage = (message) => {
+        this.setState({message})
+        setTimeout(
+            () => this.setState({message: ''}),
+            2000
+        );
     }
 
     handledEmptySubmit = (event) => {
@@ -55,18 +64,30 @@ class App extends Component {
         this.setState({
             todos: updateTodos
         })
+        saveTodo(findById(id, updateTodos))
+            .then(()=>{this.showTempMessage('Toggled')})
     }
 
-    handleClick = (id) => {
+    handleClick = (id, event) => {
+        event.preventDefault()
         const updateTodos = deleteTodo(this.state.todos, id)
         this.setState({
             todos: updateTodos
         })
+        destroyTodo(id)
+            .then(() => this.showTempMessage('Todo deleted'))
+
+    }
+
+    componentDidMount() {
+        loadTodos()
+            .then(todos => this.setState({todos}))
     }
 
     render() {
         const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handledEmptySubmit
         const displayTodos = filterTodo(this.state.todos, this.context.route)
+
         return (
             <div className="App">
                 <div className="App-header">
@@ -75,7 +96,10 @@ class App extends Component {
                 </div>
 
                 <div className="Todo-App">
-                    {this.state.errorMessage ? <span className="red">{this.state.errorMessage}</span> : ''}
+
+                    {this.state.errorMessage ? <span className="error">{this.state.errorMessage}</span> : ''}
+                    {this.state.message ? <span className="success">{this.state.message}</span> : ''}
+
                     <TodoForm
                         handleInputChange={this.handleInputChange}
                         currentTodo={this.state.currentTodo}
