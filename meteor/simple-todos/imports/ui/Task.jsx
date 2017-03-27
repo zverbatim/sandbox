ad2d4d6dbb4efc380a78f39fc817e726ef82cb8e
IM <ivan.munteanu@gmail.com>
2017-03-27 11:48:09 -0500

    meteor tut

---
+++ meteor/simple-todos/imports/ui/Task.jsx
@@ -0,0 +1,39 @@
+import React, {Component, PropTypes} from 'react';
+import { Meteor } from 'meteor/meteor';
+
+export default class Task extends Component {
+    toggleChecked() {
+        Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
+    }
+
+    deleteThisTask() {
+        Meteor.call('tasks.remove', this.props.task._id);
+    }
+
+    render() {
+        const taskClassName = this.props.task.checked ? 'checked' : '';
+
+        return (
+            <li className={taskClassName}>
+                <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+                    &times;
+                </button>
+
+                <input
+                    type="checkbox"
+                    readOnly
+                    checked={this.props.task.checked}
+                    onClick={this.toggleChecked.bind(this)}
+                />
+
+                <span className="text">
+                    <strong>{this.props.task.username}</strong>: {this.props.task.text}
+                </span>
+            </li>
+        );
+    }
+}
+
+Task.propTypes = {
+    task: PropTypes.object.isRequired,
+};
