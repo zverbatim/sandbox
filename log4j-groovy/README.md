55ac29c29cae299efbf01e1b562976083fb029a0
Ivan <ivan.munteanu@gmail.com>
2017-08-24 14:20:09 -0500

    log4j-groovy

---
+++ log4j-groovy/README.md
@@ -0,0 +1,16 @@
+# log4j demo using .properties file
+
+## requirements
+- java 1.8
+- gradle 3.5
+
+```
+# to build
+gradle clean && gradle build
+
+# to run
+java -cp build/libs -jar build/libs/log4j-groovy.jar
+
+# check the logs
+cat /var/log/log4j/log4j.log
+```
---
+++ log4j-groovy/src/main/groovy/Main.groovy
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+@groovy.transform.CompileStatic
+public class Main {
+    private final static Log logger = LogFactory.getLog(Main.class);
+
+    public static void main(String[] args) {
+        System.out.println("Working");
+        logger.info("dummy info message");
+        logger.error("dummy error message");
+        logger.debug("dummy debugg message");
+
+    }
+}
