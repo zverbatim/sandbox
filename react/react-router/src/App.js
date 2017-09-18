9af2ebb6580ceb5f58afa34ec35c534f394a780b
Ivan <ivan.munteanu@gmail.com>
2017-09-18 10:12:25 -0500

    initial

---
+++ react/react-router/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        working
+      </div>
+    );
+  }
+}
+
+export default App;
