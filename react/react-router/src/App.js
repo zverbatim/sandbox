c09d2143cab17473e140078b297f0fd0b25323a0
Ivan <ivan.munteanu@gmail.com>
2017-09-18 11:12:36 -0500

    add <Media query>

--- react/react-router/src/App.js
+++ react/react-router/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Link, Route}  from "react-router-dom";
+import {BrowserRouter, Link, Redirect, Route}  from "react-router-dom";
+import Media from 'react-media'
 import v4 from 'uuid'
 
 
@@ -52,8 +53,22 @@ const InvoiceLayout = () => (
   <div>
     <h3>Invoices</h3>
     <InvoiceNav {...data}/>
+    <Media query="(max-width: 599px)">
+      {matches => matches ? (
+        <div>
+          <p>The document is less than 600px wide.</p>
+          <Route path="/invoice/:id" component={InvoiceDetail}/>
+        </div>
+      ) : (
+        <div>
+          <p>The document is at least 600px wide. Will display the dashboard also</p>
+          <Route path="/invoice/:id" component={InvoiceDetail}/>
+          <Redirect from="/invoice" to="/invoice/dashboard/"/>
+        </div>
+      )}
+    </Media>
 
-    <Route path="/invoice/:id" component={InvoiceDetail}/>
+
   </div>
 )
 
@@ -70,15 +85,25 @@ const InvoiceNav = (props) => (
   </ul>
 )
 
-const InvoiceDetail = ({match}) => {
-  const invoice = data['invoice'].filter(it => it.id === match.params.id)[0]
-  return (
-    <nav>
-      <h3>Invoice for company <em>{invoice.title}</em></h3>
-      <p>{invoice.value}</p>
-    </nav>
-  )
-}
+const InvoiceDashboard = () => <p> this is a invoice dashboard</p>
 
+const InvoiceDetail = ({match}) => {
+  const p = match.params.id
+  if (p === 'dashboard') {
+    return (<InvoiceDashboard/>)
+  } else {
+    const invoice = data['invoice'].filter(it => it.id === match.params.id)[0]
+    return (
+      <div>{
+        invoice && (<div>
+            <h3>Invoice for company <em>{invoice.title}</em></h3>
+            <p>{invoice.value}</p>
+          </div>
+        )
+      }
+      </div>
+    )
+  }
+}
 
 export default App;
