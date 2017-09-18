f5d43d0dcfff8a8dce69b5dd9ad842042ecc5643
Ivan <ivan.munteanu@gmail.com>
2017-09-18 10:22:52 -0500

    BrowserRouter, Route, Link, key api

--- react/react-router/src/App.js
+++ react/react-router/src/App.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import {BrowserRouter, Link, Route}  from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        working
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <InvoiceApp/>
+    </BrowserRouter>
+  </div>
+)
 
+const InvoiceApp = () => (
+  <div>
+    <nav>
+      <Link to="/dashboard">Dashboard</Link>
+    </nav>
+    <div>
+      <Route path="/dashboard" component={Dashboard}/>
+    </div>
+  </div>
+)
+
+
+const Dashboard = () => (
+  <div>...this is a dashboard</div>
+)
+
+const Invoice = () => (
+  <div>... this is an invoice</div>
+)
 export default App;
