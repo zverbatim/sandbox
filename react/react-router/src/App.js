e7acca129f65d20c7e5474cd53d6d609c01e2a77
Ivan <ivan.munteanu@gmail.com>
2017-09-18 10:49:34 -0500

    nested and dynamic routes

--- react/react-router/src/App.js
+++ react/react-router/src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter, Link, Route}  from "react-router-dom";
+import v4 from 'uuid'
+
+
+const data = {
+  invoice: [
+    {
+      id: '1',
+      title: 'foo',
+      value: `$ ${Math.round(Math.random() * 1000)}.00`
+    },
+    {
+      id: '2',
+      title: 'bar',
+      value: `$ ${Math.round(Math.random() * 1000)}.00`
+    }
+  ]
+}
+
 
 const App = () => (
   <div>
@@ -14,9 +32,13 @@ const InvoiceApp = () => (
   <div>
     <nav>
       <Link to="/dashboard">Dashboard</Link>
+      {' | '}
+      <Link to="/invoice">Invoices</Link>
+      <hr/>
     </nav>
     <div>
       <Route path="/dashboard" component={Dashboard}/>
+      <Route path="/invoice" component={InvoiceLayout}/>
     </div>
   </div>
 )
@@ -26,7 +48,37 @@ const Dashboard = () => (
   <div>...this is a dashboard</div>
 )
 
-const Invoice = () => (
-  <div>... this is an invoice</div>
+const InvoiceLayout = () => (
+  <div>
+    <h3>Invoices</h3>
+    <InvoiceNav {...data}/>
+
+    <Route path="/invoice/:id" component={InvoiceDetail}/>
+  </div>
 )
+
+const InvoiceNav = (props) => (
+  <ul>
+    {
+      props.invoice.map(it =>
+        <li key={v4()}>
+          <Link to={`/invoice/${it.id}`}>
+            invoice {it.id}
+          </Link>
+        </li>)
+    }
+  </ul>
+)
+
+const InvoiceDetail = ({match}) => {
+  const invoice = data['invoice'].filter(it => it.id === match.params.id)[0]
+  return (
+    <nav>
+      <h3>Invoice for company <em>{invoice.title}</em></h3>
+      <p>{invoice.value}</p>
+    </nav>
+  )
+}
+
+
 export default App;
