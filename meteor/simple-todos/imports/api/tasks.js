26b5a203d836172e887a8108d38d40b07e599542
Ivan <ivan.munteanu@gmail.com>
2017-12-08 10:55:13 -0600

    meteor tasks

--- meteor/simple-todos/imports/api/tasks.js
+++ meteor/simple-todos/imports/api/tasks.js
@@ -1,41 +1,41 @@
-import { Mongo } from 'meteor/mongo';
-import { check } from 'meteor/check';
-import { Meteor } from 'meteor/meteor';
+import {Mongo} from "meteor/mongo";
+import {check} from "meteor/check";
+import {Meteor} from "meteor/meteor";
 
 export const Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isServer) {
-    // This code only runs on the server
-    Meteor.publish('tasks', function tasksPublication() {
-        return Tasks.find();
-    });
+  // This code only runs on the server
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find();
+  });
 }
 
 Meteor.methods({
-    'tasks.insert'(text) {
-        check(text, String);
+  'tasks.insert'(text) {
+    check(text, String);
 
-        // Make sure the user is logged in before inserting a task
-        if (! Meteor.userId()) {
-            throw new Meteor.Error('not-authorized');
-        }
+    // Make sure the user is logged in before inserting a task
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
 
-        Tasks.insert({
-            text,
-            createdAt: new Date(),
-            owner: Meteor.userId(),
-            username: Meteor.user().username,
-        });
-    },
-    'tasks.remove'(taskId) {
-        check(taskId, String);
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
+    });
+  },
+  'tasks.remove'(taskId) {
+    check(taskId, String);
 
-        Tasks.remove(taskId);
-    },
-    'tasks.setChecked'(taskId, setChecked) {
-        check(taskId, String);
-        check(setChecked, Boolean);
+    Tasks.remove(taskId);
+  },
+  'tasks.setChecked'(taskId, setChecked) {
+    check(taskId, String);
+    check(setChecked, Boolean);
 
-        Tasks.update(taskId, { $set: { checked: setChecked } });
-    },
+    Tasks.update(taskId, {$set: {checked: setChecked}});
+  },
 });
