82f2551c56cde8075c00bf130c63f42c2a847704
Ivan <ivan.munteanu@gmail.com>
2017-12-08 10:55:04 -0600

    play with the nested routes

--- react/react-router-redux/src/index.js
+++ react/react-router-redux/src/index.js
@@ -27,8 +27,42 @@ const store = createStore(
 )
 
 const Home = () => <p>Home</p>
+
 const About = () => <p>About</p>
-const Topics = () => <p>Topics</p>
+
+const Topic = ({ match }) =>
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+
+const Topics = ({match}) =>
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+
+
 
 const Navigation = () => (
   <nav>
